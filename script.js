67cfa910e4af444287012e1e28cd3029b69f9616
latinogan <roger.castillo.arce@gmail.com>
2022-05-28 06:20:52 -0500

    added js file and function to the button to show video

---
+++ script.js
@@ -0,0 +1,10 @@
+
+ function show() {
+    document.querySelector('.videos')
+            .style.display = "block";
+         
+    document.querySelector('.btn')
+           .style.display = "none";     
+}
+
+ 
